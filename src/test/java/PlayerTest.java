eb452b9cfe4566a8d006731ddf1ef3964d5da897
jesus2102 <jesusperezmarin@gmail.com>
2018-06-27 11:22:33 +0100

    PlayerTest 1 passed

---
+++ src/test/java/PlayerTest.java
@@ -0,0 +1,27 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PlayerTest {
+
+    Player player;
+    Deck deck;
+    Card card;
+
+    @Before
+    public void before(){
+        player = new Player("Fred");
+        deck = new Deck();
+
+        deck.addCards();
+    }
+
+    @Test
+    public void hasCard(){
+        assertEquals(0, player.cardCount());
+    }
+
+
+
+}
